4445b3d973789978001a74c006ec598b9631044f
Whiggsy <128292907+Whiggsy@users.noreply.github.com>
2023-03-23 00:28:44 +0000

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,5 @@
+name = input("What is your name?\n")
+print(name)
+age = input("How old are you?\n")
+print(age)
+print("Hello World!")
---
+++ menu.py
@@ -0,0 +1,7 @@
+item1 = input("what is favourite food from the menu? \n")
+item2 = input("What is you 2nd favourite food?\n")
+item3 = input("and your 3rd is?\n")
+print("Order confirmed! You have ordered:")
+print(item1)
+print(item2)
+print(item3)
